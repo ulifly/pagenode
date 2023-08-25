431ae39b8541de47d6b760a57493ab5333dd0639
Ulises Desentis <ulisesdesentis@MacBook-Air-de-Ulises.local>
2023-08-25 11:44:44 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import ejs from 'ejs';
+
+import { dirname, join } from 'path';
+
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000);
+console.log('servidor a la escucha en http://localhost:3000 🥱');
